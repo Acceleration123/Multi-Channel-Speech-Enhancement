6c15d8fd9e38081dcdbb6f844c549d5c74021d14
Acceleration123 <1497929914@qq.com>
2025-04-25 23:02:55 +0800

    Add files via upload

---
+++ csv_path.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from tqdm import tqdm
+import argparse
+
+
+def path_converter(old_path_csv, new_path):
+    old_path_df = pd.read_csv(old_path_csv)
+    new_path_df = []
+
+    for i in tqdm(range(len(old_path_df))):
+        old_path_linux = old_path_df['file_dir'][i]
+        old_path_windows = old_path_linux.replace('/', '\\')
+        new_path_windows = old_path_windows.replace('\\data\\ssd1', new_path)
+        new_path_df.append({
+            'file_dir': new_path_windows
+        })
+
+    new_path_df = pd.DataFrame(new_path_df)
+    new_path_df.to_csv(old_path_csv.replace('.csv', '_new.csv'))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-old', '--old_path_csv', type=str)
+    parser.add_argument('-new', '--new_path', type=str)
+    args = parser.parse_args()
+
+    path_converter(args.old_path_csv, args.new_path)
+
